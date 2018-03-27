27aef763aac22eb4940bb9ad63f3d3330fb7b461
tuwh <461175170@qq.com>
2018-03-27 18:53:40 +0800

    整合velocity

--- src/main/java/com/uncub/blog/user/controller/UserContorller.java
+++ src/main/java/com/uncub/blog/user/controller/UserContorller.java
@@ -5,11 +5,15 @@ import com.uncub.blog.dto.base.User;
 import com.uncub.blog.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/user")
 @Controller
@@ -38,4 +42,15 @@ public class UserContorller {
     public List<User> viewUserList(@NotNull User user) {
         return userServic.queryUser(user);
     }
+
+    @RequestMapping("/viewUser1")
+    public String viewUser1(@NotNull User user, Model model) {
+        user = userServic.queryUser(user).get(0);
+        Map map = new HashMap();
+        map.put("user",user);
+        model.addAllAttributes(map);
+        return "/viewUser2";
+    }
+
+
 }
